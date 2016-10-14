465237f82dad4648511e5cd317424219534cf4d3
venkat <venkata.amukapati@harman.com>
2016-10-14 13:34:13 +0530

    updated logs in hm

--- prj/src/java/com/i10n/db/entity/rowmapper/HardwareModuleRowMapper.java
+++ prj/src/java/com/i10n/db/entity/rowmapper/HardwareModuleRowMapper.java
@@ -3,12 +3,13 @@ package com.i10n.db.entity.rowmapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.apache.log4j.Logger;
 import org.springframework.jdbc.core.RowMapper;
 
 import com.i10n.db.entity.HardwareModule;
 
 public class HardwareModuleRowMapper implements RowMapper{
-
+	private static Logger LOG = Logger.getLogger(HardwareModuleRowMapper.class);
 	@Override
 	public Object mapRow(ResultSet rs, int rownum) throws SQLException {
 		HardwareModule hm = new HardwareModule(rs.getLong("id"),
@@ -25,6 +26,7 @@ public class HardwareModuleRowMapper implements RowMapper{
 												rs.getString("simid"),
 												rs.getString("simprovider")
 												);
+		LOG.info("Load hardware "+hm);
 		return hm;
 	}
 
